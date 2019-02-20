47e709ed7c12423cf7910edcced9061792b1656b
yewon <angelwon53@gmail.com>
2019-02-21 04:54:34 +0900

    added view , modify + add post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    # ^ : 시작 , url에 post문자포함, pk변수에 값을 넣어 뷰에 전송, d: 숫자0~9
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
