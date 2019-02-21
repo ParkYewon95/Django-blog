5324ee567918548075906eaff9a85a281cfe7fa0
yewon <angelwon53@gmail.com>
2019-02-22 01:10:54 +0900

    post new 수정

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    post = get_object_or_404(Post, pk=pk)
+    #post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
